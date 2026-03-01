a0afd68b8d9459b4d418be6346cbb9daf9db381e
Robert Breutzmann <robert.breutzmann@gmail.com>
2026-03-01 09:56:28 -0600

    Update Assignment 9.3 Final

--- module-9/RBreutzmann_Mod9_3.js
+++ module-9/RBreutzmann_Mod9_3.js
@@ -1,14 +1,14 @@
 // Declare variables
-let word1 = "Kitness";
-let word2 = "Azrial";
-let word3 = "Rite";
-let word4 = "Ren Felis";
+let word1 = "Elara";
+let word2 = "Havenshire";
+let word3 = "Ethal";
+let word4 = "accomplice";
 
 // Store the blank values
-let blank1 = "________";
-let blank2 = "_______";
-let blank3 = "____";
-let blank4 = "____ ______";
+let blank1 = "_____";
+let blank2 = "__________";
+let blank3 = "_____";
+let blank4 = "__________";
 
 // Functions to replace text or replace with blank space.
 // Toggle Town Name
@@ -24,10 +24,10 @@ function toggleWord1(button) {
     spans.forEach(span => {
         if (span.textContent === blank1) {
             span.textContent = word1;
-            button.textContent = "Hide Town Name";
+            button.textContent = "Hide Primary Character Name";
         } else {
             span.textContent = blank1;
-            button.textContent = "Reveal Town Name";
+            button.textContent = "Reveal Primary Character Name";
         }
     });
 }
@@ -39,10 +39,10 @@ function toggleWord2(button) {
     spans.forEach(span => {
         if (span.textContent === blank2) {
             span.textContent = word2;
-            button.textContent = "Hide Secondary Character Name";
+            button.textContent = "Hide Home Country Name";
         } else {
             span.textContent = blank2;
-            button.textContent = "Reveal Secondary Character Name";
+            button.textContent = "Reveal Home Country Name";
         }
     });
 }
@@ -54,10 +54,10 @@ function toggleWord3(button) {
     spans.forEach(span => {
         if (span.textContent === blank3) {
             span.textContent = word3;
-            button.textContent = "Hide Ritual Name";
+            button.textContent = "Hide Protectorate Country Name";
         } else {
             span.textContent = blank3;
-            button.textContent = "Reveal Ritual Name";
+            button.textContent = "Reveal Protectorate Country Name";
         }
     });
 }
@@ -69,10 +69,10 @@ function toggleWord4(button) {
     spans.forEach(span => {
         if (span.textContent === blank4) {
             span.textContent = word4;
-            button.textContent = "Hide Main Character Name";
+            button.textContent = "Hide Main Character's Crime";
         } else {
             span.textContent = blank4;
-            button.textContent = "Reveal Main Character Name";
+            button.textContent = "Reveal Main Character's Crime";
         }
     });
 }
