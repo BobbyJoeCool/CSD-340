f931fc8bb72da9abbb4282e4956ae634283f83b4
Robert Breutzmann <robert.breutzmann@gmail.com>
2026-03-05 14:00:29 -0600

    Upload Assignment 10.3

---
+++ module-10/RBreutzmann_Mod10_3.js
@@ -0,0 +1,11 @@
+/*
+    Breutzmann, Robert
+    CSD 340 - Web Development with HTML and CSS
+    Assignment 10.3
+    Due Date: March 8th, 2026
+*/
+
+/* Activates the "flip card" animation in the CSS" */
+function flipCard(card) {
+    card.classList.toggle("flipped");
+}
